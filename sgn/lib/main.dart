1b57813915068bf3355a72ef301b34f798cb6148
renancaraujo <renan.araujo@dashdash.com>
2020-04-29 16:26:53 +0100

    hide statusbar

--- sgn/lib/main.dart
+++ sgn/lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sgn/screens/home_screen/home_screen.dart';
 import 'package:sgn/stores/news_store.dart';
 import 'package:sgn/theme.dart';
 
-
 void main() {
-  runApp( MultiProvider(
-    providers: [Provider(create: (_) => new NewsFeedStore())],
-    child: SgnApp(),
-  ),);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+  runApp(
+    MultiProvider(
+      providers: [Provider(create: (_) => new NewsFeedStore())],
+      child: SgnApp(),
+    ),
+  );
 }
 
 class SgnApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: theme,
       title: "SGN - Some good news",
       initialRoute: '/',
@@ -23,7 +29,5 @@ class SgnApp extends StatelessWidget {
         '/': (context) => HomeScreen(),
       },
     );
-
   }
 }
-
